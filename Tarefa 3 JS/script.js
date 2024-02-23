ccb2798f4a7de38aeca2bbc25ff1d45d77e64c61
Talles-Zardo <talzardo@gmail.com>
2024-02-23 15:27:40 -0300

    Versão com Modal

--- Tarefa 3 JS/script.js
+++ Tarefa 3 JS/script.js
@@ -1,8 +1,23 @@
 let url = 'https://restcountries.com/v3.1/all';
 
+let openModal = (
+    countryName,
+    countryContinent,
+    countryCapital,
+    countryPopulation,
+    countryArea
+) => {
+    document.getElementById("modaltext").innerText = "País: " + countryName;
+    document.getElementById("modaltext2").innerText = `Continente: ${countryContinent}`;
+    document.getElementById("modaltext3").innerText = `Capital: ${countryCapital}`;
+    document.getElementById("modaltext4").innerText = `População: Cerca de ${countryPopulation} Habitantes`;
+    document.getElementById("modaltext5").innerText = `Área: Cerca de ${countryArea} km²`;
+};
+
 let list = () => {
     fetch(url)
         .then((response) => response.json())
+        
         .then((data) => {
             let row = document.querySelector('.row')
             let newHTML = ``
@@ -17,11 +32,12 @@ let list = () => {
                 //Lingua: country.languages
                 //população: country.population
                 //moeda: country.currencies
-                
+                //continente: country.continents
+
                 const country = data[i];
 
                 newHTML += ` 
-                <div style="cursor: default;" onmouseover="this.style.cursor='pointer';" onmouseout="this.style.cursor='default';" class="card col-3" onclick="alert('Capital: ${country.capital}, Área: ${country.area} km², População: ${country.population} Habitantes')">
+                <div style="cursor: default;" onmouseover="this.style.cursor='pointer';" onmouseout="this.style.cursor='default';" class="card col-3" onclick="openModal('${country.translations.por.common}', '${country.continents}', '${country.capital}', '${country.population}', '${country.area}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
                 <div class="card-body">
                     <h5 class="card-title">${country.translations.por.common}</h5>
@@ -34,9 +50,7 @@ let list = () => {
             }
             row.innerHTML = newHTML;
         });
-
 }
-
 
 function search() {
     let searchValue = document.getElementById('searchInput').value.toLowerCase();
@@ -51,6 +65,5 @@ function search() {
         }
     })
 }
-
 
 list();
